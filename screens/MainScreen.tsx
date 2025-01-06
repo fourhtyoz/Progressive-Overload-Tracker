81fd676a1a04fb9580ee3a393fd94f371bea1365
fourhtyoz <fourhtyoz@gmail.com>
2025-01-06 19:23:44 +0300

    images + backgroundImages for cards

--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, View, Text, StyleSheet, Dimensions } from "react-native";
+import { SafeAreaView, View, Text, StyleSheet, Dimensions, Pressable, ImageBackground } from "react-native";
 import { DrawerScreenProps } from "@react-navigation/drawer";
 import { DrawerParamList } from "@/navigation/DrawerNavigator";
 
@@ -9,46 +9,56 @@ type Props = DrawerScreenProps<DrawerParamList, 'Main'>;
 export default function MainScreen({ navigation }: Props) {
     return (
         <SafeAreaView style={s.wrapper}>
-            <View style={s.firstSecondWrapper}>
-                <View style={s.firstColumn}></View>
-                    <View style={s.firstBox}>
-                        <Text>1</Text>
-                    </View>
+            <View style={s.top}>
+                <View></View>
+                    <ImageBackground source={require('@/src/images/cards/result.jpg')} style={s.cardImage}>
+                        <Pressable style={s.firstBox}  onPress={() => navigation.navigate('Home')}>
+                            <Text style={s.cardTitle}>Add a result</Text>
+                        </Pressable>
+                    </ImageBackground>
                 <View style={s.secondColumn}>
-                    <View style={s.secondBox}>
-                        <Text>2</Text>
-                    </View>
-                    <View style={s.thirdBox}>
-                        <Text>3</Text>
-                    </View>
-                    <View style={s.forthBox}>
-                        <Text>4</Text>
-                    </View>
+                    <ImageBackground source={require('@/src/images/cards/about.jpg')} style={s.cardImage}>
+                        <Pressable style={s.secondBox} onPress={() => navigation.navigate('About us')}>
+                            <Text style={s.cardTitle}>About us</Text>
+                        </Pressable>
+                    </ImageBackground>
+                    <ImageBackground source={require('@/src/images/cards/profile.jpg')} style={s.cardImage}>
+                        <Pressable style={s.thirdBox} onPress={() => navigation.navigate('Profile')}>
+                            <Text style={s.cardTitle}>Profile</Text>
+                        </Pressable>
+                    </ImageBackground>
+                    <ImageBackground source={require('@/src/images/cards/settings.jpg')} style={s.cardImage}>
+                        <Pressable style={s.forthBox} onPress={() => navigation.navigate('Settings')}>
+                            <Text style={s.cardTitle}>Settings</Text>
+                        </Pressable>
+                    </ImageBackground>
                 </View>
             </View>
-                <View style={s.thirdColumn}>
-                    <View style={s.fifthBox}>
-                        <Text>5</Text>
-                    </View>
-                </View>
+            <View style={s.bottom}>
+                <ImageBackground source={require('@/src/images/cards/history.jpg')} style={s.cardImage}>
+                    <Pressable style={s.fifthBox} onPress={() => navigation.navigate('History')}>
+                        <Text style={s.cardTitle}>History</Text>
+                    </Pressable>
+                </ImageBackground>
+            </View>
         </SafeAreaView>
     )
 }
 
-const { height: screenHeight } = Dimensions.get('window');
+const { height: screenHeight, width: screenWidth } = Dimensions.get('window');
 const s = StyleSheet.create({
     wrapper: {
         marginVertical: 10
     },
-    firstSecondWrapper: {
+    top: {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
         paddingVertical: 15,
         marginHorizontal: 10
     },
-    firstColumn: {
-
+    bottom: {
+        marginHorizontal: 10
     },
     secondColumn: {
         display: 'flex',
@@ -56,33 +66,41 @@ const s = StyleSheet.create({
         gap: 10,
         marginLeft: 10
     },
-    thirdColumn: {
-        marginHorizontal: 10
-    },
     firstBox: {
         height: screenHeight / 2.5,
         flexGrow: 1,
-        backgroundColor: '#000',
         borderRadius: 15,
+        width: screenWidth / 2.2
     },
     secondBox: {
         flexGrow: 1,
         borderRadius: 15, 
-        backgroundColor: '#333'
+        height: screenHeight / 2.5 / 3
     },
     thirdBox: {
         flexGrow: 1,
         borderRadius: 15,
-        backgroundColor: '#000'
+        height: screenHeight / 2.5 / 3
     },
     forthBox: {
         flexGrow: 1,
         borderRadius: 15,
-        backgroundColor: '#000'
+        height: screenHeight / 2.5 / 3
     },
     fifthBox: {
-        backgroundColor: '#000',
         borderRadius: 15,
         height: screenHeight / 2.5,
+    },
+    cardTitle: {
+        color: '#FFF',
+        padding: 10,
+        fontSize: 18,
+        backgroundColor: 'rgba(0, 0, 0, .4)',
+        fontWeight: 'bold'
+    },
+    cardImage: {
+        overflow: 'hidden',
+        borderRadius: 15,
+        resizeMode: 'cover',
     }
 })
