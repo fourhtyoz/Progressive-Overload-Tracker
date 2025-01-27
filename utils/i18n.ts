abfe06702c8ed8dc06651ff45696e3b7cb7711b1
fourhtyoz <fourhtyoz@gmail.com>
2025-01-27 14:09:05 +0300

    getUserLanguage

--- utils/i18n.ts
+++ utils/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import * as RNLocalize from 'react-native-localize';
+import * as Localization from 'expo-localization';
 
 // translations
 import en from '@/utils/translations/en.json';
@@ -10,32 +10,42 @@ import ru from '@/utils/translations/ru.json';
 import tr from '@/utils/translations/tr.json';
 
 const resources = {
-  en: {
-    translation: en,
-  },
-  es: {
-    translation: es,
-  },
-  de: {
-    translation: de
-  },
-  ru: {
-    translation: ru
-  },
-  tr: {
-    translation: tr
-  },
+    en: {
+        translation: en,
+    },
+    es: {
+        translation: es,
+    },
+    de: {
+        translation: de
+    },
+    ru: {
+        translation: ru
+    },
+    tr: {
+        translation: tr
+    },
 };
 
+const getDeviceLanuguage = () => {
+    try {
+        const locales = Localization.getLocales()
+        const languageCode = locales[0].languageCode
+        return languageCode
+    } catch (e) {
+        console.error(`getDeviceLanuguage error: ${e}`)
+        return 'en'
+    }
+}
+
 i18n
-  .use(initReactI18next) // Passes i18n to react-i18next.
-  .init({
-    resources,
-    fallbackLng: 'en', // Default language
-    // lng: RNLocalize.getLocales()[0].languageCode, // Auto-detect device language
-    interpolation: {
-      escapeValue: false, // React already escapes values
-    },
-  });
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: getDeviceLanuguage() || 'en',
+        interpolation: {
+            escapeValue: false
+        }
+});
 
 export default i18n;
